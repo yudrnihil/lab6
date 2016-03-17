c53349bc260ba8787339c01073d44b84ecc5a898
woaitianjin <dyy_1996@sjtu.edu.cn>
2016-03-18 01:01:28 +0800

    Update main.c

--- Project/main.c
+++ Project/main.c
@@ -11,10 +11,11 @@
 
 /** @addtogroup ADC_ADC1_DMA
   * @{
-  */ 
+  */
 
 
 /* Private typedef -----------------------------------------------------------*/
+GPIO_InitTypeDef GPIO_InitStructure;
 /* Private define ------------------------------------------------------------*/
 #define ADC1_DR_Address    ((uint32_t)0x4001244C)
 #define HMC5883L_Addr	0x3C
@@ -33,6 +34,7 @@ uint16_t i;
 int16_t x;
 int16_t y;
 u8 angleChar[3];
+uint16_t segment[10]={0x01F8,0x00c0,0x01b4,0x01e4,0x00e4,0x016c,0x017c,0x01c0,0x01fc,0x01ec};
 
 /* Private function prototypes -----------------------------------------------*/
 void RCC_Configuration(void);
@@ -49,7 +51,7 @@ uint16_t getHMCAngle();
   */
 int main(void)
 {
-  
+
   SystemInit();
 
   /* Enable FSMC, GPIOA, GPIOD, GPIOE, GPIOF, GPIOG and AFIO clocks */
@@ -57,16 +59,25 @@ int main(void)
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_FSMC, ENABLE);
 
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOD |
-                         RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF | 
+                         RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF |
                          RCC_APB2Periph_GPIOG | RCC_APB2Periph_AFIO, ENABLE);
-  
-  STM3210E_LCD_Init(); 
+
+  STM3210E_LCD_Init();
 
 
   RCC_Configuration();    // RCC Init
   I2C_EE_Init();          // I2C Init
   HMC_Init();             // HMC Init
   
+  //pin config
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_3
+                              | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 ;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+
   while (1)
   {
     /* Please add code below to complete the LAB6 */
@@ -78,11 +89,11 @@ int main(void)
     LCD_DrawString(0, 0, "Angle:", 6);
     LCD_DrawString(2, 0, angleChar, 3);
     
-	
+    GPIOG->ODR=segment[angle%10-1];
 
 
-    /* main code ends here */    	
-    Delayms(100); 
+    /* main code ends here */
+    Delayms(100);
   }
 }
 
@@ -90,7 +101,7 @@ void HMC_Init(void)
 {
   I2C_ByteWrite(HMC5883L_Addr, 0x00, 0x70);
   I2C_ByteWrite(HMC5883L_Addr, 0x01, 0xA0);
-  Delayms(10); 
+  Delayms(10);
 }
 
 
@@ -106,20 +117,20 @@ void RCC_Configuration(void)
   HSEStartUpStatus = RCC_WaitForHSEStartUp();
 
   if(HSEStartUpStatus == SUCCESS)
-  { 
+  {
     /* HCLK = SYSCLK */
-    RCC_HCLKConfig(RCC_SYSCLK_Div1); 
-  
+    RCC_HCLKConfig(RCC_SYSCLK_Div1);
+
     /* PCLK2 = HCLK */
-    RCC_PCLK2Config(RCC_HCLK_Div1); 
+    RCC_PCLK2Config(RCC_HCLK_Div1);
 
     /* PCLK1 = HCLK/2 */
     RCC_PCLK1Config(RCC_HCLK_Div2);
 
     /* ADCCLK = PCLK2/4 */
-    RCC_ADCCLKConfig(RCC_PCLK2_Div4); 
-  
-#ifndef STM32F10X_CL  
+    RCC_ADCCLKConfig(RCC_PCLK2_Div4);
+
+#ifndef STM32F10X_CL
     /* PLLCLK = 8MHz * 7 = 56 MHz */
     RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_7);
 
@@ -136,12 +147,12 @@ void RCC_Configuration(void)
     while (RCC_GetFlagStatus(RCC_FLAG_PLL2RDY) == RESET)
     {}
 
-    /* PLL configuration: PLLCLK = (PLL2 / 5) * 7 = 56 MHz */ 
+    /* PLL configuration: PLLCLK = (PLL2 / 5) * 7 = 56 MHz */
     RCC_PREDIV1Config(RCC_PREDIV1_Source_PLL2, RCC_PREDIV1_Div5);
     RCC_PLLConfig(RCC_PLLSource_PREDIV1, RCC_PLLMul_7);
 #endif
 
-    /* Enable PLL */ 
+    /* Enable PLL */
     RCC_PLLCmd(ENABLE);
 
     /* Wait till PLL is ready */
@@ -181,7 +192,7 @@ void RCC_Configuration(void)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
 }
 #endif
 
@@ -193,13 +204,13 @@ void LongDelay(u32 nCount)
 void Delayms(u32 m)
 {
   u32 i;
-  
-  for(; m != 0; m--)	
+
+  for(; m != 0; m--)
        for (i=0; i<50000; i++);
 }
 
 /**
- *get x, y value from HMC5883 and calculate the angle. The angle is in degrees 
+ *get x, y value from HMC5883 and calculate the angle. The angle is in degrees
  *0 ~ 359
  */
 uint16_t getHMCAngle(){
@@ -222,5 +233,5 @@ uint16_t getHMCAngle(){
   }
   return (uint16_t)angle;
 }
-  
+
 /******************* (C) COPYRIGHT 2009 STMicroelectronics *****END OF FILE****/
